c9ee2896915cc7a733627a71a788e00e73bc3aa2
epooren <mr_zhangjiayong@163.com>
2017-05-21 22:42:50 +0800

    发布

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+PUBLIC_URL='/admin-blog.daifee.com'
+export PUBLIC_URL
+
 npm run build-css
 react-scripts build
 
 cp -r ./build ./docs
 
-# master:remote/master
+master:remote/master
+git add --all
+git commit -m '发布'
 git push
