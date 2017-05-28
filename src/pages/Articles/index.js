fa005b55e3023d3451980d0dc96b5fbe1dc36762
zhang_jia_yong <zhangjiayong@zuzuche.com>
2017-05-28 14:11:50 +0800

    增加搜索表单

--- src/pages/Articles/index.js
+++ src/pages/Articles/index.js
@@ -3,7 +3,7 @@
  *
  */
 import React from 'react';
-import {message} from 'antd';
+import {message, Form, Input, Select, Button} from 'antd';
 import ContentContainer from '../../components/ContentContainer';
 import Authorization from '../../components/Authorization';
 import ArticleList from '../../components/ArticleList';
@@ -23,6 +23,10 @@ class Articles extends React.Component {
     history.push(to(`/articles?page=${page}&perPage=${perPage}`));
   };
 
+  handleSubmit = () => {
+    console.log('submit');
+  };
+
   requestPageData() {
     let {page, perPage} = getQueryPagination({
       page: 1,
@@ -37,10 +41,36 @@ class Articles extends React.Component {
   render() {
     let {list, session, history, location} = this.props;
     let {page, perPage, data, status} = list;
-
+    // userId, title, status
     return (
       <Authorization session={session} history={history}>
         <ContentContainer title='文章列表' location={location} history={history}>
+          <Form layout='inline' onSubmit={this.handleSubmit}>
+            <Form.Item label='用户ID'>
+              <Input
+                type='text'
+                placeholder='用户ID' />
+            </Form.Item>
+
+            <Form.Item label='文章标题'>
+              <Input
+                type='text'
+                placeholder='文章标题' />
+            </Form.Item>
+
+            <Form.Item label='文章状态'>
+              <Select placeholder='选择文章状态' style={{width: '100px'}}>
+                <Option value="published">published</Option>
+                <Option value="unpublished">unpublished</Option>
+                <Option value="deleted">deleted</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">搜 索</Button>
+            </Form.Item>
+          </Form>
+
           <ArticleList
             data={data}
             page={page}
