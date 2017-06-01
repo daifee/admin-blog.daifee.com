5c61640baf1abc197a9c29a55c9e3a940999c479
daifee <mr_zhangjiayong@163.com>
2017-06-01 14:08:40 +0800

    发布

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 PUBLIC_URL='/admin-blog.daifee.com'
 export PUBLIC_URL
 
-node run build
+npm run build
 
 rm -rf ./docs
 cp -r ./build ./docs
 cp ./docs/index.html ./docs/404.html
 
-master:remote/master
+# master:remote/master
 git add --all
 git commit -m '发布'
 git push
