e48b8ce28a8b26d336953eb5731f2ca723f029f7
daifee <mr_zhangjiayong@163.com>
2017-06-01 12:02:09 +0800

    修改自定义配置

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ PUBLIC_URL='/admin-blog.daifee.com'
 export PUBLIC_URL
 
 npm run build-css
-react-scripts build
+node scripts/build.js
+# react-scripts build
 
 rm -rf ./docs
 cp -r ./build ./docs
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -125,11 +125,11 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
 
-        // query: {
-        //   plugins: [
-        //     ['import', { libraryName: 'antd', style: 'css' }],
-        //   ]
-        // }
+        query: {
+          plugins: [
+            ['import', { libraryName: 'antd', style: 'css' }],
+          ]
+        }
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
